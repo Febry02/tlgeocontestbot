6c6208d61dd4803f0cf8c93af49d5cbd86c0a2b9
saul <saul@email>
2020-08-20 17:59:26 +0300

    fix handlers

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ def main():
     dp.add_error_handler(handlers.error)
     dp.add_handler(MessageHandler(filters=Filters.status_update.new_chat_members, callback=handlers.new_chat_members))
     dp.add_handler(CommandHandler(command='balance', filters=Filters.private, callback=handlers.balance))
-    dp.add_handler(CommandHandler(command='tip', filters=~Filters.private, callback=handlers.balance))
+    dp.add_handler(CommandHandler(command='tip', filters=~Filters.private, callback=handlers.tip))
 
     dp.add_handler(ConversationHandler(
         entry_points=[
