49f80d7580eead13def8eca028b63e3bd8c1af2e
saul <saul@email>
2020-08-24 17:11:36 +0300

    withdraw

--- eth.py
+++ eth.py
@@ -1,9 +1,10 @@
 import logging
 from web3 import Web3, HTTPProvider
 
-#import settings
+import settings
 
 log = logging.getLogger(__name__)
+
 
 def make_transaction(to, value, private_key):
     w3 = Web3(HTTPProvider(settings.NODE_URL))
@@ -12,7 +13,7 @@ def make_transaction(to, value, private_key):
         dict(
             nonce=w3.eth.getTransactionCount(w3.eth.coinbase),
             gasPrice=w3.eth.gasPrice,
-            gas=100000,
+            gas=10000,
             to=to,
             value=value,
             data=b''
@@ -25,8 +26,3 @@ def make_transaction(to, value, private_key):
     except Exception as e:
         return e
     return result
-
-w3 = Web3(HTTPProvider('http://34.90.131.163:7545'))
-print(w3.eth.account.from_key('F22C1F9EDF3AE0C58667C773B8AE339669055631D466CCB4F95C404E49D9B62').address)
-print(w3.eth.getBalance('0xbFa087bE3D97BA04F33B50B62F31B2bBdC4F14dB'))
-print(w3.eth.getBalance('0x5b4509bBD2DE2B622fa3b5f2A331b3490C9D24d3'))
