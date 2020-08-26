3d699ab72e8f7fb1e21d4346fe1e80351b2c6210
saul <saul@email>
2020-08-26 09:14:28 +0300

    withdraw

--- eth.py
+++ eth.py
@@ -24,7 +24,6 @@ def test():
     w3 = Web3(HTTPProvider(settings.HTTP_NODE_URL))
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-    #from_addr = settings.TOKEN_HOLDER_ADDRESS
     from_addr = w3.eth.account.from_key(settings.PRIVATE_KEY).address
     to_addr = '0x28e0e76b14A6f3f2d351FF6cdeA0BC46c5BD091E'
 
@@ -37,7 +36,7 @@ def test():
         {
         'chainId': 1,
         'gas': 70000,
-        'gasPrice': w3.toWei('1', 'gwei'),
+        'gasPrice': 0,
         'nonce': w3.eth.getTransactionCount(contract.address)
         }
     )
