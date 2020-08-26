7034d52903db05fed951ad7e0f1fa14b5df2f2c3
saul <saul@email>
2020-08-26 09:29:38 +0300

    withdraw

--- eth.py
+++ eth.py
@@ -8,16 +8,30 @@ log = logging.getLogger(__name__)
 
 
 def make_transaction(to, value):
-    w3 = Web3(HTTPProvider(settings.HTTP_NODE_URL))
-    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-
     try:
+        w3 = Web3(HTTPProvider(settings.HTTP_NODE_URL))
+        w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+        from_addr = w3.eth.account.from_key(settings.PRIVATE_KEY).address
+
         contract = w3.eth.contract(settings.CONTRACT_ADDRESS, abi=settings.CONTRACT_ABI)
-        tx_hash = contract.functions.transfer(to, value).transact({'from': settings.TOKEN_HOLDER_ADDRESS})
-        tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+
+        txn = contract.functions.transfer(to, value).buildTransaction(
+            {
+                'from': from_addr,
+                'gas': 10000000,
+                'gasPrice': 0,
+                'nonce': w3.eth.getTransactionCount(from_addr, 'pending')
+            }
+        )
+
+        log.debug('Building new transaction: {}'.format(txn))
+
+        signed_txn = w3.eth.account.sign_transaction(txn, private_key=settings.PRIVATE_KEY)
+        hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+        return hash.hex()
     except Exception as e:
         return e
-    return tx_receipt
 
 
 def test():
@@ -50,5 +64,3 @@ def test():
 
     print(contract.functions.balanceOf(from_addr).call())
     print(contract.functions.balanceOf(to_addr).call())
-
-test()
